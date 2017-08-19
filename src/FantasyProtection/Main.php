29ae96aa5b0f6f3cb97a2be4aaca17ad527f162c
Enrick3344 <enrico3815@hotmail.fr>
2017-08-19 19:46:43 +0000

    Add files via upload

--- src/FantasyProtection/Main.php
+++ src/FantasyProtection/Main.php
@@ -10,38 +10,70 @@ use pocketmine\event\block\BlockPlaceEvent;
 use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\event\player\PlayerExhaustEvent;
 use pocketmine\event\player\PlayerDropItemEvent;
-use pocketmine\event\player\PLayerInteractEvent;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat;
+//Plugin File
+use FantasyProtection\Command;
+
 
 class Main extends PluginBase implements Listener {
 	
 	public function onEnable(){
-		if(!is_dir($this->getDataFolder())) mkdir($this->getDataFolder());
-		$this->config = new Config($this->getDataFolder()."config.yml", Config::YAML, array(
-		'Prefix' => "&7[&dFantasyProtection&7]",
-		'Hunger' => array(
-		           'world'),
-		'Break' => array(
-		          'world'),
-		'Break-Message' => "&cYou are not aloud to break blocks here!",
-		'Place' => array(
-		          'world'),
-		'Place-Message' => "&cYou are not aloud to place blocks here!",
-		'Drop' => array(
-			     'world'),
-		'Drop-Message' => "&cYou are not aloud to drop items or blocks here!"
-		));
-		$this->config->save();
+		$this->loadCommand();
+		if(!file_exists($this->getDataFolder() . "config.yml")){
+     			 @mkdir($this->getDataFolder());
+     			 file_put_contents($this->getDataFolder()."config.yml", $this->getResource("config.yml"));
+   		 }
 		$this->getServer()->getPluginManager()->registerEvents($this,$this);
 		$this->getLogger()->notice("FantasyProtection Enabled!");;
 	}
+	
+	public function onDisable(){
+		$this->getLogger()->notice("FantasyProtection Disabled!");
+	}
+	
+	public function loadCommand(){
+		$commands = [
+		"protection" => new Command($this)
+		];
+		foreach($commands as $name => $class){
+			$this->getServer()->getCommandMap()->register($name, $class);
+		}
+	}
+	
+	public function translateColors($string){
+		$msg = str_replace("&1",TextFormat::DARK_BLUE,$string);
+		$msg = str_replace("&2",TextFormat::DARK_GREEN,$msg);
+		$msg = str_replace("&3",TextFormat::DARK_AQUA,$msg);
+		$msg = str_replace("&4",TextFormat::DARK_RED,$msg);
+		$msg = str_replace("&5",TextFormat::DARK_PURPLE,$msg);
+		$msg = str_replace("&6",TextFormat::GOLD,$msg);
+		$msg = str_replace("&7",TextFormat::GRAY,$msg);
+		$msg = str_replace("&8",TextFormat::DARK_GRAY,$msg);
+		$msg = str_replace("&9",TextFormat::BLUE,$msg);
+		$msg = str_replace("&0",TextFormat::BLACK,$msg);
+		$msg = str_replace("&a",TextFormat::GREEN,$msg);
+		$msg = str_replace("&b",TextFormat::AQUA,$msg);
+		$msg = str_replace("&c",TextFormat::RED,$msg);
+		$msg = str_replace("&d",TextFormat::LIGHT_PURPLE,$msg);
+		$msg = str_replace("&e",TextFormat::YELLOW,$msg);
+		$msg = str_replace("&f",TextFormat::WHITE,$msg);
+		$msg = str_replace("&o",TextFormat::ITALIC,$msg);
+		$msg = str_replace("&l",TextFormat::BOLD,$msg);
+		$msg = str_replace("&r",TextFormat::RESET,$msg);
+		return $msg;
+	}
+	
+	
+	//PROTECTION EVENTS.
 	
 	public function onExhaust(PlayerExhaustEvent $event){
 		$player = $event->getPlayer();
 		$world = $player->getLevel()->getName();
 		$hunger = $this->getConfig()->get("Hunger");
-
         if(in_array($world, $hunger)){
              $event->setCancelled(true);
 	    }
@@ -55,11 +87,11 @@ class Main extends PluginBase implements Listener {
 		$break = $this->getConfig()->get("Break");
 		
 		if(in_array($world, $break)){
-			if($player->hasPermission("fantasyprotection.break.bypass")){
+			if($player->hasPermission("fantasyplus.break.bypass")){
 				return true;
 			}else{
 			$event->setCancelled();
-			$player->sendMessage($prefix . " " . $message);
+			$player->sendMessage($this->translateColors($prefix . " " . $message));
 			}
 		}
 	}
@@ -72,11 +104,11 @@ class Main extends PluginBase implements Listener {
 		$place = $this->getConfig()->get("Place");
 		
 		if(in_array($world, $place)){
-			if($player->hasPermission("fantasyprotection.break.bypass")){
+			if($player->hasPermission("fantasyprotection.place.bypass")){
 				return true;
 			}else{
 			$event->setCancelled();
-			$player->sendMessage($prefix . " " . $message);
+			$player->sendMessage($this->translateColors($prefix . " " . $message));
 			}
 		}
 	}
@@ -86,19 +118,31 @@ class Main extends PluginBase implements Listener {
 		$message = $this->getConfig()->get("Drop-Message");
 		$player = $event->getPlayer();
 		$world = $player->getLevel()->getName();
-		$place = $this->getConfig()->get("Drop");
+		$drop = $this->getConfig()->get("Drop");
 		
-		if(in_array($world, $place)){
+		if(in_array($world, $drop)){
 			if($player->hasPermission("fantasyprotection.drop.bypass")){
 				return true;
 			}else{
 			$event->setCancelled();
-			$player->sendMessage($prefix . " " . $message);
+			$player->sendMessage($this->translateColors($prefix . " " . $message));
 			}
 		}
 	}
 	
-	public function onDisable(){
-		$this->getLogger()->notice("FantasyProtection Disabled!");
+	public function onHurt(EntityDamageEvent $event){
+		if($event->getEntity() instanceof Player && $event instanceof EntityDamageByEntityEvent) {
+			if($event->getDamager() instanceof Player){
+				$prefix = $this->getConfig()->get("Prefix");
+				$message = $this->getConfig()->get("PVP-Message");
+				$pvp = $this->getConfig()->get("PVP");
+				$player = $event->getDamager();
+				$world = $player->getLevel()->getName();
+				if(in_array($world, $pvp)){
+					$event->getDamager()->sendMessage($this->translateColors($prefix . " " . $message));
+					$event->setCancelled();
+				}
+			}
+		}
 	}
 }
